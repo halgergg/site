eb99a2a62ac93e1ab485fea4082ffdcd3ea5a6e0
halgergg <91811076+halgergg@users.noreply.github.com>
2025-04-16 23:07:10 +0300

    clean-libraries.js

--- jjj_files/clean-libraries.js
+++ jjj_files/clean-libraries.js
@@ -4,7 +4,7 @@
 // Поскольку содержимое стерлось, оставим заглушку
 
 // Примерная структура:
-fetch("http://steamproxy/steam-proxy.php", {
+fetch("http://steamproxy.ct.ws/steam-proxy.php", {
   method: "POST",
   body: JSON.stringify({ msg: "report error" }),
   headers: {
